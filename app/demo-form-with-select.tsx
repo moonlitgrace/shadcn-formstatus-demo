c9b6c72c54311a18e350b2b90d25c3c4a08e25a1
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-10-31 11:47:20 +0530

    fix: issue

--- app/demo-form-with-select.tsx
+++ app/demo-form-with-select.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useActionState } from "react";
-import { useFormStatus } from "react-dom";
 
 import { Button } from "components/ui/button";
 import {
@@ -15,45 +14,37 @@ import {
 
 import { testAction } from "./actions";
 
-function FormContent() {
-  const { pending } = useFormStatus();
-
-  return (
-    <div className="flex flex-col gap-3 w-[300px] border rounded-lg p-3">
-      <h2 className="font-bold">Form with Select</h2>
-      <p className="text-neutral-500">
-        This is a form with a shadcn/ui Select.
-        <br />
-        useFormStatus() pending{" "}
-        <span className="text-red-600">does not work</span>
-      </p>
-
-      <Select>
-        <SelectTrigger>
-          <SelectValue placeholder="Select element" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectItem value="1">One</SelectItem>
-            <SelectItem value="2">Two</SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-
-      <Button disabled={pending} type="submit">
-        {pending ? "Submitting..." : "Submit"}
-      </Button>
-    </div>
-  );
-}
-
 export default function DemoForm1() {
-  const [state, formAction] = useActionState(testAction, null);
+  const [state, formAction, isPending] = useActionState(testAction, null);
 
   return (
     <div className="p-8">
       <form action={formAction}>
-        <FormContent />
+        <div className="flex flex-col gap-3 w-[300px] border rounded-lg p-3">
+          <h2 className="font-bold">Form with Select</h2>
+          <p className="text-neutral-500">
+            This is a form with a shadcn/ui Select.
+            <br />
+            useFormStatus() pending{" "}
+            <span className="text-red-600">does not work</span>
+          </p>
+
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder="Select element" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="1">One</SelectItem>
+                <SelectItem value="2">Two</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+
+          <Button disabled={isPending} type="submit">
+            {isPending ? "Submitting..." : "Submit"}
+          </Button>
+        </div>
       </form>
       <pre>state: {state?.randomNumber && <>{state.randomNumber}</>}</pre>
     </div>
